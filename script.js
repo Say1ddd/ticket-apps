51e49b604c1d16a45b13f5f0d3ff938509e68558
Say1ddd <sajidmuhammadikhlas@gmail.com>
2023-02-26 14:14:33 +0700

    Add: break loop

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 function useTicket() {
     while(true){
         const nama = prompt("Masukkan nama:");
+        if (nama === null || nama === "") break;
+
         const umur = prompt("Masukkan umur:");
+        if (umur === null || umur === "") break;
+
         if (umur > 13){
-            const studio = prompt("Pilih studio: \n[A] Studio A \n[B] Studio B \n[C] Studio C").toUpperCase();
-            if (studio === "A"){
+            const studio = prompt("Pilih studio: \n[A] Studio A \n[B] Studio B \n[C] Studio C");
+            if (studio === null || studio === "") break;
+
+            const upperStudio = studio.toUpperCase();
+            if (upperStudio === "A"){
                 alert(`Tiket ${nama}, Studio A, Umur ${umur}`);
-            } else if (studio === "B"){
+            } else if (upperStudio === "B"){
                 alert(`Tiket ${nama}, Studio B, Umur ${umur}`);
-            } else if (studio === "C"){
+            } else if (upperStudio === "C"){
                 alert(`Tiket ${nama}, Studio C, Umur ${umur}`);
             } else {
                 alert("tidak boleh masuk studio");
