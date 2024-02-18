7f904a8cbc30ddfece2ddaecdc950841bec408db
Say1ddd <sajidmuhammadikhlas@gmail.com>
2024-02-18 14:06:08 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const nama = prompt("Masukkan nama:");
+const umur = prompt("Masukkan umur:");
+if (umur > 13){
+    const studio = prompt("Pilih studio: \n[A] Studio A \n[B] Studio B \n[C] Studio C");
+    if (studio === "A"){
+        alert(`Tiket ${nama}, Studio A, Umur ${umur}`);
+    } else if (studio === "B"){
+        alert(`Tiket ${nama}, Studio B, Umur ${umur}`);
+    } else if (studio === "C"){
+        alert(`Tiket ${nama}, Studio C, Umur ${umur}`);
+    } else {
+        alert("tidak boleh masuk studio");
+    }
+} else {
+    alert("tidak boleh masuk studio");
+}
